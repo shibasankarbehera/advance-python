b932bf9804b15fab4e9c3322d84a35bbc884f3e9
shibasankarbehera <sivasankarbrank@gmail.com>
2026-03-03 17:51:06 +0530

    Add files via upload

---
+++ krishna.py
@@ -0,0 +1,14 @@
+#So this Code is Dedicated to Kana urf Krishna
+def krishna(name):
+    if name == "kana":
+        print("Hello, Kana Welcome to Epstein Island")
+        return
+    elif name == "rehman":
+        print("Hello Rehaman Welcome to Lyari")
+        return
+    else:
+        print("Hamza Fucking Rehman's(Kana Bhai) wife")
+print("Select one of the following names")
+print("1.kana\n2.rehman")
+nm = input("Enter Krishna's Name: ")
+krishna(nm)
